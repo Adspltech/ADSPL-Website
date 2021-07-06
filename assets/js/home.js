bc27fe75b308cbac206895db45449568fe3b23fc
pratik-adspl <86817808+pratik-adspl@users.noreply.github.com>
2021-07-06 18:07:31 +0530

    path changed

---
+++ assets/js/home.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.readmorebutton').click(function () {
+        console.log('innnnnn');
+        var collapse_content_selector = $(this).attr('href');
+        var toggle_switch = $(this);
+        $(collapse_content_selector).toggle(function () {
+            if ($(this).css('display') == 'none') {
+                toggle_switch.html('Read More');
+            } else {
+                toggle_switch.html('Read Less');
+            }
+        });
+    });
+
+});
