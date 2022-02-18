f2c5ab915bc414c50496a4d186dbe2d85709f4c0
Hemmyhtec Creatives <76248385+hemmyhtec@users.noreply.github.com>
2022-02-18 05:41:26 -0800

    Update tabs.js

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -3973,7 +3973,7 @@ $.extend(Datepicker.prototype, {
 			inst.append.remove();
 		}
 		if (appendText) {
-			inst.append = $("<span class='" + this._appendClass + "'>" + appendText + "</span>");
+			inst.append = $("<span class=" + this._appendClass + ">" + appendText + "</span>");
 			input[isRTL ? "before" : "after"](inst.append);
 		}
 
